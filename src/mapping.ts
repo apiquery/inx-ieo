c52cb736bab82e192d3f4c3c2e87592e3c3e591b
cryptmarcus <cryptmarcus+apiquery@pm.me>
2020-12-09 13:46:41 +0100

    created handleTransfer function

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,7 +6,57 @@ import {TransferCounter} from './entities/TransferCounter';
 import {TotalSupply} from './entities/TotalSupply';
 
 export function handleTransfer(event: Transfer): void {
+    let day = (event.block.timestamp / BigInt.fromI32(60 * 60 * 24));
 
+    let holderFrom = Holder.load(event.params.from.toHex());
+    if (holderFrom == null) {
+        holderFrom = newHolder(event.params.from.toHex(), event.params.from.toHex());
+    }
+    holderFrom.balance = holderFrom.balance - event.params.value;
+    holderFrom.transactionCount = holderFrom.transactionCount + 1;
+    holderFrom.save();
+
+    let holderTo = Holder.load(event.params.to.toHex());
+    if (holderTo == null) {
+        holderTo = newHolder(event.params.to.toHex(), event.params.to.toHex());
+
+        // HolderCounter
+        let holderCounter = HolderCounter.load('singleton');
+        if (holderCounter == null) {
+            holderCounter = new HolderCounter('singleton');
+            holderCounter.count = 1;
+        } else {
+            holderCounter.count = holderCounter.count + 1;
+        }
+        holderCounter.save();
+        holderCounter.id = day.toString();
+        holderCounter.save();
+    }
+    holderTo.balance = holderTo.balance + event.params.value;
+    holderTo.transactionCount = holderTo.transactionCount + 1;
+    holderTo.save();
+
+    // Transfer counter total and historical
+    let transferCounter = TransferCounter.load('singleton')
+    if (transferCounter == null) {
+        transferCounter = new TransferCounter('singleton')
+        transferCounter.count = 0
+        transferCounter.totalTransferred = BigInt.fromI32(0)
+    }
+    transferCounter.count = transferCounter.count + 1
+    transferCounter.totalTransferred = transferCounter.totalTransferred + event.params.value
+    transferCounter.save()
+    transferCounter.id = day.toString()
+    transferCounter.save()
+}
+
+function newHolder(id: string, address: string): Holder {
+    let holder = new Holder(id);
+    holder.address = address;
+    holder.balance = BigInt.fromI32(0);
+    holder.transactionCount = 0;
+    holder.isContract = false;
+    return holder;
 }
 
 /*export function handleRestrictionsUpdated(event: RestrictionsUpdated): void {
